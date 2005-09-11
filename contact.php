73ff3bc66334fd366f68b97faba19e65025f085e
bbateman <bbateman>
2005-09-11 21:21:57 +0000

    Changed code over to support objects in theme compatibility

--- contact.php
+++ contact.php
@@ -48,29 +48,35 @@
 			global $lang_string, $user_colors, $logged_in, $theme_vars, $blog_theme;	
 			$entry_array = array();
 			$entry_array[ 'subject' ] = $lang_string[ 'title' ];
-			$entry_array[ 'entry' ] = $lang_string[ 'instructions' ] . '<p />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<form accept-charset="' . $lang_string[ 'html_charset' ] . ',iso-8859-1,utf-8" action="contact_cgi.php" method="POST" onSubmit="return validate(this)">';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<label for="name">' . $lang_string[ 'name' ] . '</label><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<input type="text" name="name" size="40"><br /><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<label for="email">' . $lang_string[ 'email' ] .'</label><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<input type="text" name="email" size="40"><br /><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<label for="subject">' . $lang_string[ 'subject' ] . '</label><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<input type="text" name="subject" size="40"><br /><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<label for="comment">' . $lang_string[ 'comment' ] . '</label><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<textarea style="width: ' . $theme_vars[ 'max_image_width' ] . 'px;" id="text" name="comment" rows="20" cols="50" autocomplete="OFF"></textarea><br /><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<label for="capcha_contact">';
-			if ( function_exists('imagecreate') ) {
-				$tempcapcha = $lang_string[ 'contact_capcha' ] . '<br /><img src="capcha.php?entry=contact" />';
-			}
-			else {
-				$tempcapcha = $lang_string[ 'contact_capcha' ] . '<b>' . sb_str_to_ascii( $_SESSION[ 'capcha_contact' ] . '</b>');
-			}	
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . $tempcapcha;
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '</label><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<input type="text" name="capcha_contact" value="" autocomplete="OFF" maxlength="6"><br /><br />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '<input type="submit" name="submit" value="' . $lang_string[ 'submit_btn' ] . '" />';
-			$entry_array[ 'entry' ] = $entry_array[ 'entry' ] . '</form>';
 			
+			ob_start(); ?>
+			<?php echo ( $lang_string[ 'instructions' ] ); ?><p />
+			<form accept-charset="<?php echo( $lang_string[ 'html_charset' ] ); ?>,iso-8859-1,utf-8" action="contact_cgi.php" method="POST" onSubmit="return validate(this)">
+		
+			<label for="name"><?php echo( $lang_string[ 'name' ] ); ?></label><br />
+			<input type="text" name="name" size="40"><br /><br />
+			<label for="email"><?php echo( $lang_string[ 'email' ] ); ?></label><br />
+			<input type="text" name="email" size="40"><br /><br />
+			<label for="subject"><?php echo( $lang_string[ 'subject' ] ); ?></label><br />
+			<input type="text" name="subject" size="40"><br /><br />
+			<label for="comment"><?php echo( $lang_string[ 'comment' ] ); ?></label><br />
+			<textarea style="width: <?php global $theme_vars; echo( $theme_vars[ 'max_image_width' ] ); ?>px;" id="text" name="comment" rows="20" cols="50" autocomplete="OFF"></textarea><br /><br />
+			<label for="capcha_contact"><?php 
+				if ( function_exists('imagecreate') ) {
+					echo ( $lang_string[ 'contact_capcha' ] . '<br /><img src="capcha.php?entry=contact" />' );
+				}
+				else {
+					echo ( $lang_string[ 'contact_capcha' ] . '<b>' . sb_str_to_ascii( $_SESSION[ 'capcha_contact' ] ) . '</b>' );
+				}
+			?></label><br />
+			<input type="text" name="capcha" value="" autocomplete="OFF" maxlength="6"><br /><br />
+			<hr noshade size="1" color="#<?php echo( $user_colors[ 'inner_border_color' ] ); ?>" />
+	
+			<input type="submit" name="submit" value="<?php echo( $lang_string[ 'submit_btn' ] ); ?>" />
+			</form>
+			<?php
+			$entry_array[ 'entry' ] = ob_get_contents();
+			ob_end_clean();
 			echo( theme_staticentry( $entry_array ) );	
 		} 
 	}
