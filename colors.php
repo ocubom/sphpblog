df234fcd290c665ea31f0c36718b9a468d57c139
jgutierrez <jgutierrez>
2005-09-15 23:06:34 +0000

    Upgraded flash applet to version 8

--- colors.php
+++ colors.php
@@ -99,7 +99,7 @@
 		?>
 		
 		<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" 
-			codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" 
+			codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" 
 			width="390" height="390" id="color_picker" align="">
 			<param name=movie value="flash/color_picker.swf"> 
 			<param name=quality value=high> 
