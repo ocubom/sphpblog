a8ea833b022e539a8590ddeea61408947ca40ec5
apalmo <apalmo>
2005-12-05 19:34:34 +0000

    Added some comments

--- rss.php
+++ rss.php
@@ -13,6 +13,12 @@
 	require_once('languages/' . $blog_config[ 'blog_language' ] . '/strings.php');
 	sb_language( 'index' );
 	
+	// Please note, you can change the number of entries that appear by
+	// passing the variable 'n' in the URL. You can also limit it to a certain
+	// category by passing 'c'. For instance, this will make it show
+	// 20 entries in the 'tech' category:
+	// http://www.mywebsite.com/rss.php?n=20&c=tech
+	
 	// Output Page
 	generate_rss( @$_GET[ 'n' ], @$_GET[ 'c' ] );
 ?>
