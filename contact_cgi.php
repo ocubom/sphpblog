e9cee9ab89eaa5f3b5aac468b9f6ba7edfd8328d
apalmo <apalmo>
2006-04-22 15:16:59 +0000

    Fixed broken head tag

--- contact_cgi.php
+++ contact_cgi.php
@@ -42,7 +42,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-head>
+<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo( $lang_string[ 'html_charset' ] ); ?>" />
 	<link rel="stylesheet" type="text/css" href="themes/<?php echo( $blog_theme ); ?>/style.css" />
 	<?php require_once('themes/' . $blog_theme . '/user_style.php'); ?>
