5383c9dd016721afae4f823f997bb5572fa41fc5
apalmo <apalmo>
2006-05-04 04:55:55 +0000

    Added Simple theme.

---
+++ themes/simple/user_style.php
@@ -0,0 +1,60 @@
+<style type="text/css">
+	body {
+		background-color: #<?php echo( $user_colors[ 'bg_color' ] ); ?>;
+		color: #<?php echo( $user_colors[ 'txt_color' ] ); ?>;
+	}
+
+	hr	
+	{
+		color: #<?php echo( $user_colors[ 'inner_border_color' ] ); ?>;
+		background-color: #<?php echo( $user_colors[ 'inner_border_color' ] ); ?>;
+	}
+
+	#header_image {
+		border-color: #<?php echo( $user_colors[ 'border_color' ] ); ?>;
+	}
+
+	#header {
+		border-color: #<?php echo( $user_colors[ 'border_color' ] ); ?>;
+		color: #<?php echo( $user_colors[ 'header_txt_color' ] ); ?>;
+		background-color: #<?php echo( $user_colors[ 'header_bg_color' ] ); ?>;
+	}
+
+	#footer {
+		color: #<?php echo( $user_colors[ 'footer_txt_color' ] ); ?>;
+		background: #<?php echo( $user_colors[ 'footer_bg_color' ] ); ?>;
+		border-top: 1px solid #<?php echo( $user_colors[ 'border_color' ] ); ?>;
+	}
+
+	h1, h2, h3, h4, h5, h6 {
+		color: #<?php echo( $user_colors[ 'headline_txt_color' ] ); ?>;
+	}
+
+	#maincontent .blog_subject {
+		color: #<?php echo( $user_colors[ 'headline_txt_color' ] ); ?>;
+	}
+
+	#maincontent .blog_date {
+		color: #<?php echo( $user_colors[ 'date_txt_color' ] ); ?>;
+	}
+
+	#maincontent .blog_categories {
+		color: #<?php echo( $user_colors[ 'date_txt_color' ] ); ?>;
+	}
+	
+	#sidebar .menu_body {
+		border-color: #<?php echo( $user_colors[ 'inner_border_color' ] ); ?>;
+	}
+
+	a:link, a:visited {
+		color: #<?php echo( $user_colors[ 'link_reg_color' ] ); ?>;
+	}
+
+	a:hover {
+		color: #<?php echo( $user_colors[ 'link_hi_color' ] ); ?>;
+	}
+
+	a:active {
+		color: #<?php echo( $user_colors[ 'link_down_color' ] ); ?>;
+	}
+</style>
