6b6870c23ecd7dd9f85633a4fe3da3b4c3d27069
apalmo <apalmo>
2006-12-14 04:33:13 +0000

    Added stricter validation of input. Now checks if comments are enabled, expired, ip's match, etc. Also, gives more informative error responses.

--- comment_add_cgi.php
+++ comment_add_cgi.php
@@ -4,7 +4,7 @@
 	// ---------------
 	require_once('scripts/sb_functions.php');
 	global $logged_in;
-	$logged_in = logged_in( true, true );
+	$logged_in = logged_in( false, true );
 	
 	if ( !session_id() ) {
 		session_start();
@@ -20,83 +20,242 @@
 	// ---------------
 
 	// Verify information being passed
-	global $ok;
-	$ok = false;
+	global $ok, $error_message;
+	$ok = true;
+	$error_message = '';
+	
+	/*
+			$lang_string['expired_comment1'] = "We are sorry. New comments are not allowed after "; // New for 0.4.8
+			$lang_string['expired_comment2'] = " days."; // New for 0.4.8
 
-	// Verify that all the correct POST keys exist (except comment_remember which is a check box)
-	$fieldsExist = true;
-	$requiredFields = Array( 'y', 'm', 'entry', 'comment_capcha', 'comment_name', 'comment_email', 'comment_url', 'comment_text' );
-	foreach ( $requiredFields as $fieldName ) {
-		if ( !array_key_exists( $fieldName, $_POST ) ) {
-			// echo("Field \"$fieldName\" is missing<br />");
-			$fieldsExist = false;
-			break;
+			$lang_string['blacklisted'] = "Sorry, your IP address has been banned. Comments not allowed."; // New for 0.4.8
+			$lang_string['bannedword'] = "Your comment, url, name or email contained word(s) that have been banned by the administrator. Your comment has NOT been posted."; // New for 0.4.8
+			$lang_string['nocomments'] = "Comments are not available for this entry."; // New for 0.4.9
+			$lang_string['email_moderator'] = "Comments are currently moderated. This comment needs approval before it will be seen by the public."; // New for 0.5.0
+			$lang_string['user_notice_mod'] = "Moderation is turned on for this blog. Your comment will require the administrators approval before it will be visible."; // new for 0.5.0
+			
+			// Error Response
+			$lang_string['error_add'] = "<h2>Whoops!</h2>Comment not saved. I ran into a problem while saving your comment.<br /><br />Server Reported:<br />";
+			$lang_string['error_delete'] = "<h2>Whoops!</h2>Comment not deleted. I ran into a problem while deleting your comment.<br /><br />Server Reported:<br />";
+			$lang_string['error_ban'] = "<h2>Whoops!</h2>IP not added to banned ip listing.<br /><br />Server Reported:<br />";
+			$lang_string['success_add'] = "<h2>Comment Added!</h2>Your comment has been successfully saved."; // New for 0.4.8.1
+			$lang_string['success_delete'] = "<h2>Comment Deleted!</h2>The comment has been deleted."; // New for 0.4.8.1
+			$lang_string['success_ban1'] = "<h2>IP Banned!";
+			$lang_string['success_ban2'] = "</h2>To remove this ban in the future, use the Moderation option in the preferences menu."; // New for 0.4.8.1
+			$lang_string['form_error'] = "Please complete the Name, Comment and Anti-Spam fields.";
+			$lang_string['error_noip'] = "No IP Provided for Blacklist Request.";
+			
+			$lang_string[ 'error_comments_disabled' ] = '<h2>Error!</h2>Hey! Comments are disabled on this blog. Are you a spambot?'; // New 0.5.0
+			$lang_string[ 'error_no_match' ] = '<h2>Error!</h2>You IP address doesn\t match posted IP address. Are you a spambot?'; // New 0.5.0
+			$lang_string[ 'error_fields_missing' ] = '<h2>Error!</h2>Missing the following fields: '; // New 0.5.0
+			$lang_string[ 'error_spambot' ] = '<h2>Error!</h2>Capcha data is missing. Are you a spambot?'; // New 0.5.0
+			$lang_string[ 'error_capcha' ] = '<h2>Error!</h2>The capcha you entered is incorrect.'; // New 0.5.0
+			$lang_string[ 'error_bad_data' ] = '<h2>Error!</h2>Post data is not valid. Are you a hacker?'; // New 0.5.0
+			$lang_string[ 'error_entry_missing' ] = '<h2>Error!</h2>You are trying to comment on an entry that doesn\'t exist'; // New 0.5.0
+			$lang_string[ 'error_empty_text' ] = '<h2>Error!</h2>You didn\'t enter any comments or your name.'; // New 0.5.0
+	*/
+	
+	if ($ok) {
+		// Check if comments are disabled. If so, this is probably a spam bot.
+		if ($blog_config[ 'blog_enable_comments' ] == 0) {
+			$commentsEnabled = false;
+			$ok = false;
+			$error_message = $lang_string[ 'error_comments_disabled' ];
+		} else {
+			$commentsEnabled = true;
 		}
 	}
-
-	// Do the banned words checking (New 0.4.9)
-	$oBannedWords = new CBannedWords;
-	$oBannedWords->load( 'config/bannedwordlist.txt' );
-	if ( $oBannedWords->ContainsBannedWord( $_POST[ 'comment_name' ], $_POST[ 'comment_email' ], $_POST[ 'comment_url' ], $_POST[ 'comment_text' ] ) )
-	{
-		$ok = $lang_string['bannedword'];
-	}
-
-	if (!$logged_in && $_SESSION[ 'capcha_' . $_POST['entry' ] ] == '') {
-		// Capcha did not exist in session, so comment poster did not come from comments page,
-		// where this should have been created. User is probably a spam robot.
-		// Fix submitted by Jan Normann Nielsen via Sourceforge 2006-08-11
-		$fieldsExist = false;
-	}
-
-	// Handle the moderator flag...
-	// Blank for release, H for hold comment (don't show to regular users)
-	if ( $logged_in == true ) {
-		$holdflag = '';
-	} else {
-		$holdflag = 'H';
-	}
-
-	if ( $ok != True ) {
-		if ( $fieldsExist ) {
-			// Dis-allow dots, and slashes to make sure the
-			// user is not able to back-up a directory.
-			//
-			// Make sure the string lengths are correct.
-			if ( $_POST[ 'comment_capcha' ] == $_SESSION[ 'capcha_' . $_POST[ 'entry' ] ] || $logged_in ) {
-				if ( strpos( $_POST[ 'y' ], array( '/', '.', '\\', '%' ) ) === false && strlen( $_POST[ 'y' ] ) == 2 &&
-				strpos( $_POST[ 'm' ], array( '/', '.', '\\', '%' ) ) === false && strlen( $_POST[ 'm' ] ) == 2 &&
-				strpos( $_POST[ 'entry' ], array( '/', '.', '\\', '%' ) ) === false && strlen( $_POST[ 'entry' ] ) == 18 ) {
-				
-					// Verify that the file exists.
-					if ( entry_exists ( $_POST[ 'y' ], $_POST[ 'm' ], $_POST[ 'entry' ] ) ) {
-						if ( strlen( $_POST[ 'comment_name' ] ) > 0 && strlen( $_POST[ 'comment_text' ] ) > 0 ) {
-							$ok = write_comment( $_POST[ 'y' ], $_POST[ 'm' ], $_POST[ 'entry' ],
-								sb_stripslashes( $_POST[ 'comment_name' ] ),
-								sb_stripslashes( $_POST[ 'comment_email' ] ),
-								sb_stripslashes( $_POST[ 'comment_url' ] ),
-								$_POST[ 'comment_remember' ],
-								sb_stripslashes( $_POST[ 'comment_text' ] ),
-								$_POST[ 'user_ip' ],
-								$holdflag );
-								@session_unregister( 'capcha_' . $_GET[ 'entry' ] );
-						}
-					}
-				}
+	
+	if ($ok) {
+		// Verify required fields exist (except comment_remember which is a check box...)
+		$requiredFields = Array( 'y', 'm', 'entry', 'comment_capcha', 'comment_name', 'comment_email', 'comment_url', 'comment_text', 'user_ip' );
+		$missingFields = Array();
+		foreach ( $requiredFields as $fieldName ) {
+			if ( !array_key_exists( $fieldName, $_POST ) ) {
+				array_push( $missingFields, $fieldName );
 			}
 		}
+		
+		if ( count($missingFields) == 0 ) {
+			$allFieldsExist = true;
+		} else {
+			$allFieldsExist = false;
+			$ok = false;
+			$error_message = $lang_string[ 'error_fields_missing' ] . implode ( ', ', $missingFields );
+		}
 	}
-
-	if ( $ok === true ) {
+	
+	if ($ok) {
+		// Verify that posted IP and actual IP matches.
+		if ( getIP() === $_POST['user_ip'] ) {
+			$ipMatches = true;
+		} else {
+			$ipMatches = false;
+			$ok = false;
+			$error_message = $lang_string[ 'error_no_match' ];
+		}
+	}
+	
+	if ($ok) {
+		// Check IP against Blacklist
+		$oBlacklist = new CBlacklist;
+		$oBlacklist->load( 'config/blacklist.txt' );
+		if ( $oBlacklist->isBanned( getIP() ) == true ) {
+			$notBlackListed = false;
+			$ok = false;
+			$error_message = $lang_string[ 'blacklisted' ];
+		} else {
+			$notBlackListed = true;
+		}
+	}
+	
+	if ($ok) {
+		// Validate year, month and entry data format.
+		if ( strpos( $_POST[ 'y' ], array( '/', '.', '\\', '%', '#', ';' ) ) === false && strlen( $_POST[ 'y' ] ) == 2 &&
+			strpos( $_POST[ 'm' ], array( '/', '.', '\\', '%', '#', ';' ) ) === false && strlen( $_POST[ 'm' ] ) == 2 &&
+			strpos( $_POST[ 'entry' ], array( '/', '.', '\\', '%', '#', ';' ) ) === false && strlen( $_POST[ 'entry' ] ) == 18 ) {
+			// Dis-allow dots, and slashes to make sure the user is not able to
+			// back-up a directory. Make sure the string lengths are correct.
+			
+			$postDataValid = true;
+		} else {
+			$postDataValid = false;
+			$ok = false;
+			$error_message = $lang_string[ 'error_bad_data' ];
+		}
+	}
+	
+	if ($ok) {
+		// Capcha Missing / Spambot Check
+		if ( $logged_in == false && $_SESSION[ 'capcha_' . $_POST['entry' ] ] == '' ) {
+			// Capcha did not exist in session, so comment poster did not come from the
+			// comments page, where this should have been created. User is probably a spam robot.
+			// Fix submitted by Jan Normann Nielsen via Sourceforge 2006-08-11
+			$notSpamBot = false;
+			$ok = false;
+			$error_message = $lang_string[ 'error_spambot' ];
+		} else {
+			$notSpamBot = true;
+		}
+	}
+	
+	if ($ok) {
+		// Capcha Check
+		if ( $_POST[ 'comment_capcha' ] == $_SESSION[ 'capcha_' . $_POST[ 'entry' ] ] || $logged_in ) {
+			$capchaCorrect = true;
+		} else {
+			$capchaCorrect = false;
+			$ok = false;
+			$error_message = $lang_string[ 'error_capcha' ];
+		}
+	}
+	
+	if ($ok) {
+		// Banned Word Check (New 0.4.9)
+		$oBannedWords = new CBannedWords;
+		$oBannedWords->load( 'config/bannedwordlist.txt' );
+		if ( $oBannedWords->ContainsBannedWord( $_POST[ 'comment_name' ], $_POST[ 'comment_email' ], $_POST[ 'comment_url' ], $_POST[ 'comment_text' ] ) ) {
+			$noBannedWords = false;
+			$ok = false;
+			$error_message = $lang_string[ 'bannedword' ];
+		} else {
+			$noBannedWords = true;
+		}
+	}
+	
+	if ($ok) {
+		// Entry Exists Check
+		if ( entry_exists ( $_POST[ 'y' ], $_POST[ 'm' ], $_POST[ 'entry' ] ) ) {
+			$entryExists = true;
+		} else {
+			$entryExists = false;
+			$ok = false;
+			$error_message = $lang_string[ 'error_entry_missing' ];
+		}
+	}
+	
+	if ($ok) {
+		// Check for empty name or comment text.
+		if ( strlen( $_POST[ 'comment_name' ] ) == 0 || strlen( $_POST[ 'comment_text' ] ) == 0 ) {
+			$notEmpty = false;
+			$ok = false;
+			$error_message = $lang_string[ 'error_empty_text' ];
+		} else {
+			$notEmpty = true;
+		}
+	}
+	
+	if ($ok) {
+		// Check if comment are already expired.
+		$entry = $_POST[ 'entry' ];
+		$year = substr($entry, 5, 2);
+		$month = substr($entry, 7, 2);
+		$day = substr($entry, 9, 2);
+		if ( are_comments_expired( $month, $dday, $year ) ) {
+			$notExpired = false;
+			$ok = false;
+			$error_message = $lang_string['expired_comment1'] . $blog_config[ 'blog_comment_days_expiry' ] . $lang_string['expired_comment2'];
+		} else {
+			$notExpired = true;
+		}
+		
+	}
+	
+	if ($ok) {
+		// Comment Moderation
+		if ( $logged_in == false && $blog_config[ 'blog_enable_comments' ] ) {
+			// Hold comment (don't show to regular users)
+			$moderationFlag = 'H';
+		} else {
+			// Immediate release,
+			$moderationFlag = '';
+		}
+		
+		$comment_name = sb_stripslashes( $_POST[ 'comment_name' ] );
+		$comment_email = sb_stripslashes( $_POST[ 'comment_email' ] );
+		$comment_url = sb_stripslashes( $_POST[ 'comment_url' ] );
+		$comment_text = sb_stripslashes( $_POST[ 'comment_text' ] );
+		
+		$result = write_comment( $_POST[ 'y' ], $_POST[ 'm' ], $_POST[ 'entry' ],
+			$comment_name,
+			$comment_email,
+			$comment_url,
+			$comment_text,
+			$_POST[ 'user_ip' ],
+			$moderationFlag,
+			time() );
+		
+		// Save remember me cookie
+		if ( $_POST[ 'comment_remember' ] != 0 ) {
+			setcookie( 'comment_name', $comment_name, time()+60*60*24*30);
+			setcookie( 'comment_email', $comment_email, time()+60*60*24*30);
+			setcookie( 'comment_url', $comment_url, time()+60*60*24*30);
+		}
+		
+		// Delete entry capcha session
+		@session_unregister( 'capcha_' . $_GET[ 'entry' ] );
+		
+		if ( $result === true ) {
+			// comment was successfully written!
+		} else {
+			// problem writing file or couldn't create folder.
+			$ok = false;
+			$error_message = $lang_string[ 'error_add' ] . $result;
+		}
+	}
+	
+	/*
+	if ( $ok == true ) {
 		$relative_url = 'comments.php?y='.$_POST[ 'y' ].'&m='.$_POST[ 'm' ].'&entry='.$_POST[ 'entry' ];
 		redirect_to_url( $relative_url );
 	}
+	*/
 	
 	// ------------
 	// PAGE CONTENT
 	// ------------
 	function page_content() {
-		global $lang_string, $blog_config, $ok;
+		global $lang_string, $blog_config, $logged_in, $ok, $error_message;
 	
 		// SUBJECT
 		$entry_array = array();
@@ -105,10 +264,19 @@
 		// PAGE CONTENT BEGIN
 		ob_start();
 		
-		if ( $ok !== true ) {
-			echo $lang_string[ 'error_add' ] . $ok . '<p />';
+		if ( $ok == true ) {
+			echo( $lang_string[ 'success_add' ] . '<p />' );
+			
+			if ( $blog_config[ 'blog_comments_moderation' ] ) {
+				if ( $logged_in == false ) {
+					echo($lang_string['user_notice_mod'] . '<p />');
+				}
+			}
+			
+			$relative_url = 'comments.php?y='.$_POST[ 'y' ].'&m='.$_POST[ 'm' ].'&entry='.$_POST[ 'entry' ];
+			echo( '<a href="' . $relative_url . '">' . $lang_string[ 'return_to_comments' ] . '</a><p />' );
 		} else {
-			echo $lang_string[ 'success_add' ] . '<p />';
+			echo( $error_message . '<p />' );
 		}
 		echo( '<a href="index.php">' . $lang_string[ 'home' ] . '</a>' );
 		
@@ -141,7 +309,7 @@
 		// BEGIN OUTPUT
 		// ------------
 		global $blog_config;
-		if ( $blog_config[ 'blog_comments_popup' ] == 1 ) {
+		if ( $blog_config[ 'blog_comments_popup' ] ) {
 			theme_popuplayout();
 		} else {
 			theme_pagelayout();
