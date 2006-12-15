1e61a8e04f323a23717d6fff050a6dfb8e62937b
apalmo <apalmo>
2006-12-15 19:46:04 +0000

    Removed cookie reference. Now only using sessions.

--- scripts/sb_login.php
+++ scripts/sb_login.php
@@ -1,5 +1,4 @@
 <?php 
-
 	// The Simple PHP Blog is released under the GNU Public License.
 	//
 	// You are free to use and modify the Simple PHP Blog. All changes 
@@ -9,30 +8,20 @@
 	// -------------------------
 	// Session & Login Functions
 	// -------------------------
-	
 	function logged_in ( $redirect_to_login, $redirect_to_setup ) {
-		// Check if the user is logged in and has a session.
-		//
-		// Session data is stored in a client-side cookie.
-		// Login flag is stored in the session cookie.
-
-		// Support for PHP >= 4.1.0
-		// Check if the user has a client-side cookie.
-		if ( isset( $_COOKIE[ 'my_id' ] ) ) {
-			$my_id = $_COOKIE[ 'my_id' ];
-			
-			// Turn off URL SIDs.
-			ini_set('url_rewriter.tags','');
-			ini_set('session.use_trans_sid', false);
-			
-			// Init the session.
-			session_set_cookie_params(60*60*24*5);
-			session_id($my_id);
-			session_start ();
-			
-			// Check if they're logged in.
-			if ( isset( $_SESSION[ 'logged_in' ] ) && $_SESSION[ 'logged_in' ] == 'yes' ) {
-				if ( $_SESSION[ 'site_path' ] === dirname($_SERVER[ 'PHP_SELF' ]) ) {
+		// Turn off URL SIDs.
+		ini_set('url_rewriter.tags','');
+		ini_set('session.use_trans_sid', false);
+		
+		// Init the session.
+		session_set_cookie_params(60*60*24*5);
+		session_start ();
+		
+		// Check if user is logged in.
+		if ( isset( $_SESSION[ 'logged_in' ] ) && $_SESSION[ 'logged_in' ] == 'yes' ) {
+			if ( $_SESSION[ 'site_path' ] === dirname($_SERVER[ 'PHP_SELF' ]) ) {
+				if ( $_SESSION[ 'ip' ] === getIP() ) {
+					// User is logged in.
 					return ( true );
 				}
 			}
@@ -40,7 +29,6 @@
 		
 		// Special Case:
 		// If there's no password file then need to redirect them.
-		
 		$username = null;
 		$password = null;
 		
@@ -54,7 +42,6 @@
 			
 			if ( $redirect_to_setup ) {
 				redirect_to_url( 'install00.php' );
-				
 				exit;
 			} else {
 				return ( false );
@@ -67,47 +54,109 @@
 		
 		if ( $redirect_to_login ) {
 			redirect_to_url( 'index.php' );
-			
 			exit;
 		} else {
 			return ( false );
 		}
 	}
 	
-	function logout () {
-		// Log Out
+	function logout () {			
+		// Turn off URL SIDs.
+		ini_set('url_rewriter.tags','');
+		ini_set('session.use_trans_sid', false);
 		
-		if ( isset( $_COOKIE[ 'my_id' ] ) ) {
-			$my_id = $_COOKIE[ 'my_id' ];
-			
-			// Turn off URL SIDs.
-			ini_set('url_rewriter.tags','');
-			ini_set('session.use_trans_sid', false);
-			
-			// Init the session.
-			session_set_cookie_params(60*60*24*5);
-			session_id($my_id);
-			session_start();
-			
-			// Check if they're actually logged in (for reporting...)
-			$was_logged_in = 0;
-			if ( isset( $_SESSION[ 'logged_in' ] ) && $_SESSION[ 'logged_in' ] == 'yes' ) {
-				if ( $_SESSION[ 'site_path' ] === dirname($_SERVER[ 'PHP_SELF' ]) ) {
+		// Init the session.
+		session_set_cookie_params(60*60*24*5);
+		session_start();
+		
+		// Check if user is logged in (just for reporting...)
+		$was_logged_in = 0;
+		if ( isset( $_SESSION[ 'logged_in' ] ) && $_SESSION[ 'logged_in' ] == 'yes' ) {
+			if ( $_SESSION[ 'site_path' ] === dirname($_SERVER[ 'PHP_SELF' ]) ) {
+				if ( $_SESSION[ 'ip' ] === getIP() ) {
 					$was_logged_in = 1;
 				}
 			}
-			
-			session_unset($_SESSION[ 'logged_in' ]);
-			session_unset($_SESSION[ 'site_path' ]);
-			session_destroy();
-			
-			// Delete cookie
-			setcookie('my_id','',time()-60*60);
-			
-			return ( $was_logged_in );
-		} else {
-			return ( -1 );
 		}
+		
+		session_unset($_SESSION[ 'logged_in' ]);
+		session_unset($_SESSION[ 'site_path' ]);
+		session_unset($_SESSION[ 'ip' ]);
+		session_destroy();
+		
+		return ( $was_logged_in );
+	}
+	
+	function check_password ( $user, $pass ) {
+		// Check password against hashed password file
+		$username = null;
+		$password = null;
+		
+		@include('config/password.php');
+		
+		if ( $username == null || $password == null ) {
+			// Missing password.php file...
+		} else {
+			// Verify Username
+			if ( crypt( $user, $username ) === $username ) {
+				if ( crypt( $pass, $password ) === $password ) {
+					// Start Session and Set Cookie
+					ini_set('url_rewriter.tags','');
+					ini_set('session.use_trans_sid', false);
+			
+					session_set_cookie_params(60*60*24*5);
+					@session_start();
+					
+					// Support for PHP >= 4.1.0
+					$_SESSION[ 'logged_in' ] = 'yes';
+					$_SESSION[ 'site_path' ] = dirname($_SERVER[ 'PHP_SELF' ]);
+					$_SESSION[ 'ip' ] = getIP();
+					// setcookie('my_id',session_id(),time()+60*60*24*5);
+					
+					// Clear variables (why not...)
+					$username = null;
+					$password = null;
+					
+					return ( true );
+				}
+			}
+		}
+		
+		// Clear variables (why not...)
+		$username = null;
+		$password = null;
+		
+		return ( false );
+	}
+	
+	function redirect_to_url( $relative_url = "index.php" ) {
+		// Many thanks to Ridgarou for fixing the port issue.
+	
+		if ( strpos ($_SERVER['HTTP_HOST' ], ":") != false ){
+			$port = '';
+		} else {
+			$port = ':' . $_SERVER[ 'SERVER_PORT'];
+			if ($port == ':80') {
+				$port = '';
+			}
+		}
+		
+		$proto = 'http://';
+		if ($_SERVER['HTTPS'] == 'on') {
+			$proto = 'https://';
+			if ($port == ':443') {
+				$port = '';
+			}
+		}
+
+ 		if ( ( dirname($_SERVER[ 'PHP_SELF' ]) == '\\' || dirname($_SERVER[ 'PHP_SELF' ]) == '/' ) ) {
+			// Hosted at root.
+			header('Location: '.$proto.$_SERVER['HTTP_HOST' ].$port.'/'.$relative_url);
+		} else {
+			// Hosted in sub-directory.
+			header('Location: '.$proto.$_SERVER['HTTP_HOST' ].$port.dirname($_SERVER[ 'PHP_SELF']).'/'.$relative_url);
+		}
+		exit;
 	}
 	
 	/*
@@ -170,79 +219,4 @@
 		}
 	}
 	*/
-	
-	function check_password ( $user, $pass ) {
-		// Check password against hashed password file
-		//
-		$username = null;
-		$password = null;
-		
-		@include('config/password.php');
-		
-		if ( $username == null || $password == null ) {
-			// Missing password.php file...
-		} else {
-			// Verify Username
-			if ( crypt( $user, $username ) === $username ) {
-				if ( crypt( $pass, $password ) === $password ) {
-					// Start Session and Set Cookie
-					ini_set('url_rewriter.tags','');
-					ini_set('session.use_trans_sid', false);
-			
-					session_set_cookie_params(60*60*24*5);
-					@session_start();
-					
-					// Support for PHP >= 4.1.0
-					$_SESSION[ 'logged_in' ] = 'yes';
-					$_SESSION[ 'site_path' ] = dirname($_SERVER[ 'PHP_SELF' ]);
-					setcookie('my_id',session_id(),time()+60*60*24*5);
-					
-					// Clear variables (why not...)
-					$username = null;
-					$password = null;
-					
-					return ( true );
-				}
-			}
-		}
-		
-		// Clear variables (why not...)
-		$username = null;
-		$password = null;
-		
-		return ( false );
-	}
-	
-	function redirect_to_url( $relative_url = "index.php" ) {
-		// Many thanks to Ridgarou for fixing the port issue.
-	
-		if ( strpos ($_SERVER['HTTP_HOST' ], ":") != false ){
-			$port = '';
-		  
-		} else {
-			$port = ':' . $_SERVER[ 'SERVER_PORT'];
-			
-			if ($port == ':80') {
-				$port = '';
-			}
-		}
-		
-		$proto = 'http://';
-		if ($_SERVER['HTTPS'] == 'on') {
-			$proto = 'https://';
-
-			if ($port == ':443') {
-				$port = '';
-			}
-		}
-
- 		if ( ( dirname($_SERVER[ 'PHP_SELF' ]) == '\\' || dirname($_SERVER[ 'PHP_SELF' ]) == '/' ) ) {
-			// Hosted at root.
-			header('Location: '.$proto.$_SERVER['HTTP_HOST' ].$port.'/'.$relative_url);
-		} else {
-			// Hosted in sub-directory.
-			header('Location: '.$proto.$_SERVER['HTTP_HOST' ].$port.dirname($_SERVER[ 'PHP_SELF']).'/'.$relative_url);
-		}
-		exit;
-	}
 ?>
