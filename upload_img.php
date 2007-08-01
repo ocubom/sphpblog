7392c1ff633f93e1a4cbc00465aad41c70d00eca
bbateman <bbateman>
2007-08-01 05:47:12 +0000

    Fix upload image bug where error was reported on an echo call.

--- upload_img.php
+++ upload_img.php
@@ -1,96 +1,96 @@
-<?php 
-	// ---------------
-	// INITIALIZE PAGE
-	// ---------------
-	require_once('scripts/sb_functions.php');
-	global $logged_in;
-	$logged_in = logged_in( true, true );
-	
-	read_config();
-	
-	require_once('languages/' . $blog_config[ 'blog_language' ] . '/strings.php');
-	sb_language( 'upload_img' );
-	
-	// ---------------
-	// POST PROCESSING
-	// ---------------
-	
-	// ------------
-	// PAGE CONTENT
-	// ------------
-	function page_content() {
-		global $lang_string, $blog_config;
-		
-		// SUBJECT
-		$entry_array = array();
-		$entry_array[ 'subject' ] = $lang_string[ 'title' ];
-			
-		// PAGE CONTENT BEGIN
-		ob_start(); ?>
-		
-		<?php
-		$formstate = $_REQUEST['formstate'];
-		if($formstate !="showuploaders"){
-echo( <<<EOF
-		<form action="upload_img.php" method="get">
-			How many images do you wish to upload? <select id="howmany" name="howmany">
-EOF;
-		for($i=1;$i<=10;$i++){
-			echo('<option value="'.$i.'">'.$i.'</option>');
-		}
+<?php
+  // ---------------
+  // INITIALIZE PAGE
+  // ---------------
+  require_once('scripts/sb_functions.php');
+  global $logged_in;
+  $logged_in = logged_in( true, true );
+  
+  read_config();
+  
+  require_once('languages/' . $blog_config[ 'blog_language' ] . '/strings.php');
+  sb_language( 'upload_img' );
+  
+  // ---------------
+  // POST PROCESSING
+  // ---------------
+  
+  // ------------
+  // PAGE CONTENT
+  // ------------
+  function page_content() {
+    global $lang_string, $blog_config;
+    
+    // SUBJECT
+    $entry_array = array();
+    $entry_array[ 'subject' ] = $lang_string[ 'title' ];
+      
+    // PAGE CONTENT BEGIN
+    ob_start(); ?>
+    
+    <?php
+    $formstate = $_REQUEST['formstate'];
+    if($formstate !="showuploaders"){
 echo <<<EOF
-	</select>
-	<input name="formstate" type="hidden" value="showuploaders" />
-	<input type="submit" value="next &raquo;" />
-		</form>
+    <form action="upload_img.php" method="get">
+      How many images do you wish to upload? <select id="howmany" name="howmany">
 EOF;
-		} else {
-			$howmany = $_REQUEST['howmany'];
-			$formstate = "notshowuploaders";
-			
-			echo( $lang_string[ 'instructions' ] );
-			echo("<p />");
-		
-			echo('<form enctype="multipart/form-data" action="upload_img_cgi.php" method="POST">');
-			echo( $lang_string[ 'select_file' ] );
-			echo("<br /><br />");
-			for($i=1;$i<=$howmany;$i++){
-				echo("<input name=\"userfile[]\" type=\"file\"><br />");			
-			}
-			echo("<input name=\"howmany\" type=\"hidden\" value=\"$howmany\" />");
-			echo("<input type=\"submit\" value=\"".$lang_string[ 'upload_btn' ]."\">");	
-			echo("</form>");
-		}		
-		?>
-		<?php
-		// PAGE CONTENT END
-		$entry_array[ 'entry' ] = ob_get_clean();
-		
-		// THEME ENTRY
-		echo( theme_staticentry( $entry_array ) );
-	}
-	
-	// ----
-	// HTML
-	// ----
+    for($i=1;$i<=10;$i++){
+      echo('<option value="'.$i.'">'.$i.'</option>');
+    }
+echo <<<EOF
+  </select>
+  <input name="formstate" type="hidden" value="showuploaders" />
+  <input type="submit" value="next &raquo;" />
+    </form>
+EOF;
+    } else {
+      $howmany = $_REQUEST['howmany'];
+      $formstate = "notshowuploaders";
+      
+      echo( $lang_string[ 'instructions' ] );
+      echo("<p />");
+    
+      echo('<form enctype="multipart/form-data" action="upload_img_cgi.php" method="POST">');
+      echo( $lang_string[ 'select_file' ] );
+      echo("<br /><br />");
+      for($i=1;$i<=$howmany;$i++){
+        echo("<input name=\"userfile[]\" type=\"file\"><br />");      
+      }
+      echo("<input name=\"howmany\" type=\"hidden\" value=\"$howmany\" />");
+      echo("<input type=\"submit\" value=\"".$lang_string[ 'upload_btn' ]."\">"); 
+      echo("</form>");
+    }   
+    ?>
+    <?php
+    // PAGE CONTENT END
+    $entry_array[ 'entry' ] = ob_get_clean();
+    
+    // THEME ENTRY
+    echo( theme_staticentry( $entry_array ) );
+  }
+  
+  // ----
+  // HTML
+  // ----
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo( $lang_string[ 'html_charset' ] ); ?>" />
-	
-	<link rel="stylesheet" type="text/css" href="themes/<?php echo( $blog_theme ); ?>/style.css" />
-	<?php require_once('themes/' . $blog_theme . '/user_style.php'); ?>
-	<?php require_once('scripts/sb_javascript.php'); ?>
-	<script language="javascript" src="scripts/sb_javascript.js" type="text/javascript"></script>
-	
-	<title><?php echo($blog_config[ 'blog_title' ]); ?> - <?php echo( $lang_string[ 'title' ] ); ?></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=<?php echo( $lang_string[ 'html_charset' ] ); ?>" />
+  
+  <link rel="stylesheet" type="text/css" href="themes/<?php echo( $blog_theme ); ?>/style.css" />
+  <?php require_once('themes/' . $blog_theme . '/user_style.php'); ?>
+  <?php require_once('scripts/sb_javascript.php'); ?>
+  <script language="javascript" src="scripts/sb_javascript.js" type="text/javascript"></script>
+  
+  <title><?php echo($blog_config[ 'blog_title' ]); ?> - <?php echo( $lang_string[ 'title' ] ); ?></title>
 </head>
-	<?php 
-		// ------------
-		// BEGIN OUTPUT
-		// ------------
-		theme_pagelayout();
-	?>
+  <?php 
+    // ------------
+    // BEGIN OUTPUT
+    // ------------
+    theme_pagelayout();
+  ?>
 </html>
