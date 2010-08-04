9e7bc2ca9b3bef2841d1f76b8be924120c968884
nabber00 <nabber00@739c1e2e-722b-0410-a95f-adbfc7c39bab>
2010-08-04 01:45:29 +0000

    finalized search plugin
    git-svn-id: https://svn.code.sf.net/p/sphpblog/svn/trunk@16 739c1e2e-722b-0410-a95f-adbfc7c39bab

--- plugins/search.php
+++ plugins/search.php
@@ -15,14 +15,13 @@ $thispage = curPageURL();
 
 $rootpage = dirname(dirname($thispage));
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
+echo "<?xml version=\"1.0\" ?>";
 ?>
 
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/"
                        xmlns:moz="http://www.mozilla.org/2006/browser/search/">
 <ShortName><?php echo $blog_config->getTag('BLOG_TITLE') ?></ShortName>
 <Description><?php echo $blog_config->getTag('INFO_DESCRIPTION') ?></Description>
-<InputEncoding>UTF-8</InputEncoding>
 <Image width="16" height="16" type="image/x-icon"><?php echo $rootpage ?>/interface/favicon.ico</Image>
 <Url type="text/html" template="<?php echo $rootpage ?>/search.php?q={searchTerms}" />
 <moz:SearchForm><?php echo $rootpage ?>/search.php</moz:SearchForm>
