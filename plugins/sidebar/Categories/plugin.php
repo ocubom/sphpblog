28f71f853c51b51bb21f1e1fd0150e6934263fec
nabber00 <nabber00@739c1e2e-722b-0410-a95f-adbfc7c39bab>
2010-09-14 20:17:18 +0000

    applied patch ID: 1827677 Entry Quantity in Sidebar
    git-svn-id: https://svn.code.sf.net/p/sphpblog/svn/trunk@78 739c1e2e-722b-0410-a95f-adbfc7c39bab

--- plugins/sidebar/Categories/plugin.php
+++ plugins/sidebar/Categories/plugin.php
@@ -26,9 +26,41 @@
 			
 			$catArray = get_category_array();
 			if ( count($catArray) > 0) {
+				$blogEntriesArray = blog_entry_listing();
+				$blogEntryValuesArray = array();
+				$catHasEntryArray = array();
+				
+				for ($i = 0; $i < count($catArray); $i++) {
+					$catHasEntryArray[$catArray[$i][0]] = 0;
+				}
+				
+				if ( count($blogEntriesArray) > 0) {
+					for ( $i = 0; $i < count( $blogEntriesArray ); $i++) {
+						list( $_entry_filename, $_year_dir, $_month_dir ) = explode( '|', $blogEntriesArray[$i] );
+						$blogEntryValue = blog_entry_to_array(CONTENT_DIR . $_year_dir . '/' . $_month_dir . '/' . $_entry_filename);
+						
+						if (strpos($blogEntryValue['CATEGORIES'], ',') === false) {
+							if (isset($catHasEntryArray[$blogEntryValue['CATEGORIES']])) {
+								$catHasEntryArray[$blogEntryValue['CATEGORIES']]++;
+							} else {
+								$catHasEntryArray[$blogEntryValue['CATEGORIES']] = 1;
+							}
+						} else {
+							$entryCategories = explode(',', $blogEntryValue['CATEGORIES']);
+							for ($y = 0; $y < count($entryCategories); $y++) {
+								if (isset($catHasEntryArray[$entryCategories[$y]])) {
+									$catHasEntryArray[$entryCategories[$y]]++;
+								} else {
+									$catHasEntryArray[$entryCategories[$y]] = 1;
+								}
+							}
+						}
+					}
+				}
+
 				for ( $i = 0; $i < count( $catArray ); $i++ ) {
 					$id_number = $catArray[$i][0];
-					$name_str = $catArray[$i][1];
+					$name_str = $catArray[$i][1] . " (" . $catHasEntryArray[$id_number] . ")";  //  <-- extended Category Name
 					$space_count = $catArray[$i][2];
 					for ( $j = 0; $j < $space_count; $j++ ) {
 						// Indent the proper number of spaces...
