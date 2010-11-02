0b7a4992e4e989c771344b3d0e1260f629bec1b2
nabber00 <nabber00@739c1e2e-722b-0410-a95f-adbfc7c39bab>
2010-11-02 19:10:51 +0000

    added creative commons license box for ID: 1557039
    git-svn-id: https://svn.code.sf.net/p/sphpblog/svn/trunk@121 739c1e2e-722b-0410-a95f-adbfc7c39bab

---
+++ scripts/plugins/sidebar/CreativeCommons/plugin.php
@@ -0,0 +1,180 @@
+<?php
+
+$cc['by'] = '
+<!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by/2.5/"><img alt="Creative Commons License" 
+height="31" width="88" src="http://creativecommons.org/images/public/somerights20.gif"/></a><br/>This work is licensed under a <a rel="license" 
+href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5 License</a>.<!--/Creative Commons License--><!-- 
+<rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" 
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	<Work rdf:about="">
+		<license rdf:resource="http://creativecommons.org/licenses/by/2.5/" />
+	<dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+	</Work>
+	<License rdf:about="http://creativecommons.org/licenses/by/2.5/"><permits 
+rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires 
+rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><permits 
+rdf:resource="http://web.resource.org/cc/DerivativeWorks"/></License></rdf:RDF> -->
+';
+
+$cc['by-nc'] = '
+<!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by-nc/2.5/"><img alt="Creative Commons License" 
+height="31" width="88" src="http://creativecommons.org/images/public/somerights20.gif"/></a><br/>This work is licensed under a <a rel="license" 
+href="http://creativecommons.org/licenses/by-nc/2.5/">Creative Commons Attribution-NonCommercial 2.5 License</a>.<!--/Creative Commons 
+License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" 
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	<Work rdf:about="">
+		<license rdf:resource="http://creativecommons.org/licenses/by-nc/2.5/" />
+	<dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+	</Work>
+	<License rdf:about="http://creativecommons.org/licenses/by-nc/2.5/"><permits 
+rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires 
+rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><prohibits 
+rdf:resource="http://web.resource.org/cc/CommercialUse"/><permits 
+rdf:resource="http://web.resource.org/cc/DerivativeWorks"/></License></rdf:RDF> -->';
+
+$cc['by-sa'] = '
+<!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by-sa/2.5/"><img alt="Creative Commons License" 
+height="31" width="88" src="http://creativecommons.org/images/public/somerights20.gif"/></a><br/>This work is licensed under a <a rel="license" 
+href="http://creativecommons.org/licenses/by-sa/2.5/">Creative Commons Attribution-ShareAlike 2.5 License</a>.<!--/Creative Commons 
+License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" 
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	<Work rdf:about="">
+		<license rdf:resource="http://creativecommons.org/licenses/by-sa/2.5/" />
+	<dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+	</Work>
+	<License rdf:about="http://creativecommons.org/licenses/by-sa/2.5/"><permits 
+rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires 
+rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><permits 
+rdf:resource="http://web.resource.org/cc/DerivativeWorks"/><requires 
+rdf:resource="http://web.resource.org/cc/ShareAlike"/></License></rdf:RDF> -->
+';
+
+$cc['by-nc-sa'] = '
+<!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/"><img alt="Creative Commons 
+License" height="31" width="88" src="http://creativecommons.org/images/public/somerights20.gif"/></a><br/>This work is licensed under a <a 
+rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/">Creative Commons Attribution-NonCommercial-ShareAlike 2.5 
+License</a>.<!--/Creative Commons License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" 
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	<Work rdf:about="">
+		<license rdf:resource="http://creativecommons.org/licenses/by-nc-sa/2.5/" />
+	<dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+	</Work>
+	<License rdf:about="http://creativecommons.org/licenses/by-nc-sa/2.5/"><permits 
+rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires 
+rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><prohibits 
+rdf:resource="http://web.resource.org/cc/CommercialUse"/><permits rdf:resource="http://web.resource.org/cc/DerivativeWorks"/><requires 
+rdf:resource="http://web.resource.org/cc/ShareAlike"/></License></rdf:RDF> -->
+';
+ 
+$cc['by-nd'] = '
+<!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by-nd/2.5/"><img alt="Creative Commons License" 
+height="31" width="88" src="http://creativecommons.org/images/public/somerights20.gif"/></a><br/>This work is licensed under a <a rel="license" 
+href="http://creativecommons.org/licenses/by-nd/2.5/">Creative Commons Attribution-NoDerivs 2.5 License</a>.<!--/Creative Commons 
+License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" 
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	<Work rdf:about="">
+		<license rdf:resource="http://creativecommons.org/licenses/by-nd/2.5/" />
+	<dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+	</Work>
+	<License rdf:about="http://creativecommons.org/licenses/by-nd/2.5/"><permits 
+rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires 
+rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/></License></rdf:RDF> 
+-->
+';
+
+$cc['by-nc-nd'] = '
+<!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/2.5/"><img alt="Creative Commons 
+License" height="31" width="88" src="http://creativecommons.org/images/public/somerights20.gif"/></a><br/>This work is licensed under a <a 
+rel="license" href="http://creativecommons.org/licenses/by-nc-nd/2.5/">Creative Commons Attribution-NonCommercial-NoDerivs 2.5 
+License</a>.<!--/Creative Commons License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" 
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	<Work rdf:about="">
+		<license rdf:resource="http://creativecommons.org/licenses/by-nc-nd/2.5/" />
+	<dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+	</Work>
+	<License rdf:about="http://creativecommons.org/licenses/by-nc-nd/2.5/"><permits 
+rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires 
+rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><prohibits 
+rdf:resource="http://web.resource.org/cc/CommercialUse"/></License></rdf:RDF> -->
+';
+
+	class CreativeCommons extends Sidebar {
+		var $plugin;
+		
+		/* ------ INITIALIZE ------ */
+		
+		function CreativeCommons () {
+			$this->plugin = 'CreativeCommons';
+			$this->loadPrefs();
+		}
+
+                /* ------ PREFERENCES ------ */
+
+                function defaultPrefs () {
+                        $arr = parent::defaultPrefs();
+                        $arr['license'] = 'by-nc-nd';
+
+                        return $arr;
+                }
+
+                /* ------ OPTIONS ------ */
+
+                function optionsForm () {
+                        $license = $this->prefs['license'];
+
+                        $save = $GLOBALS['lang_string']['submit_btn'];
+
+                        ob_start(); ?>
+                        <!-- FORM -->
+			<p>License</p>
+                        <form method="post" onsubmit="return validate(this)">
+				<select name="license">
+					<option value='by'>Attribution</option>
+					<option value='by-nc'>Attribution Non-Commercial</option>
+					<option value='by-sa'>Attribution Share Alike</option>
+					<option value='by-nc-sa'>Attribution Non-Commercial Share Alike</option>
+					<option value='by-nd'>Attribution No Derivatives</option>
+					<option value='by-nc-nd'>Attribution Non-Commercial No Derivatives</option>
+				</select>
+
+                                <input type="submit" name="save" value="<?php echo( $save ); ?>" />
+                        </form>
+                        <?php
+                        $str = ob_get_clean();
+
+                        return $str;
+                }
+
+                function optionsPost () {
+                        if ( array_key_exists( 'save', $_POST ) ) {
+
+				$value = '';
+                                if ( array_key_exists( 'license', $_POST ) ) {
+	                                $this->prefs['license'] = $_POST['license'];
+                                }
+
+                                $this->savePrefs();
+
+                        }
+                }
+
+		
+		/* ------ GETTERS & SETTERS ------ */
+		
+		function getTitle () {
+			return 'Creative Commons';
+		}
+		
+		function getContent () {
+			global $cc;
+
+			    // WEB BADGES
+			    $html = '<div style="text-align: center">' . "\n";
+			    if (!empty($this->prefs['license'])) 
+			    $html .= $cc[$this->prefs['license']];
+
+			    $html .= '</div>' . "\n";
+			return $html;
+		}
+	}
+?>
