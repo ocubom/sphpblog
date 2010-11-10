5769c5910ed98b6c15cca21791f7c8dd1d4e2750
nabber00 <nabber00@739c1e2e-722b-0410-a95f-adbfc7c39bab>
2010-11-10 04:33:38 +0000

    started converting pages over to new structure
    git-svn-id: https://svn.code.sf.net/p/sphpblog/svn/trunk@144 739c1e2e-722b-0410-a95f-adbfc7c39bab

--- add.php
+++ add.php
@@ -5,11 +5,19 @@
 	require_once('scripts/sb_functions.php');
 	global $logged_in;
 	$logged_in = logged_in( true, true );
-	
-	read_config();
-	
-	require_once('languages/' . $blog_config->getTag('BLOG_LANGUAGE') . '/strings.php');
-	sb_language( 'add' );
+
+	ob_start();
+        require_once('scripts/sb_editor.php');
+        $head = ob_get_clean();
+
+	$lang = 'add';
+
+	// ----
+	// HTML
+	// ----
+	include_once('scripts/sb_header.php');
+
+	include_once('scripts/sb_footer.php');
 	
 	// ---------------
 	// POST PROCESSING
@@ -56,30 +64,5 @@
 		$entry_array[ 'entry' ] = $editor['form'];
 		echo( theme_staticentry( $entry_array ) ); // THEME ENTRY
 	}
-	// ----
-	// HTML
-	// ----
 	
-	// Main Page Template
-	$page_template = new Template(TEMPLATE_DIR.'layouts/index.tpl');
-	
-	// Meta Data
-	get_init_code($page_template);
-	
-	// Extra Javascript
-	ob_start();
-	require_once('scripts/sb_editor.php');
-	$page_template->appendTag('{JAVASCRIPT}', ob_get_clean());
-	
-	// Page Title
-	$page_template->setTag('{PAGE_TITLE}', $blog_config->getTag('BLOG_TITLE').' - '.$GLOBALS['lang_string']['title']);
-	
-	// Theme Layout
-	ob_start();
-	theme_pagelayout(); 
-	$page_template->setTag('{BODY}', ob_get_clean());
-		
-	// Final Output
-	$output = $page_template->getHTML();
-	echo($output);
 ?>
