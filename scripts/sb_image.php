4ecab7c41b1f87985b75558faa3a16557ac8bb51
nabber00 <nabber00@739c1e2e-722b-0410-a95f-adbfc7c39bab>
2011-07-14 15:55:52 +0000

    added patch: thumb in show images ID: 1967454
    git-svn-id: https://svn.code.sf.net/p/sphpblog/svn/trunk@175 739c1e2e-722b-0410-a95f-adbfc7c39bab

--- scripts/sb_image.php
+++ scripts/sb_image.php
@@ -10,30 +10,39 @@
 	// Image Upload Functions
 	// ----------------------
 	
-	function image_list () {
+	
+	function image_list () 
+		{
 		// Get a list of images in the image folder. Return HTML.
 		//
-		
-		if (!file_exists(IMAGES_DIR)) {
+		// TODO 777 is bad, fix this!
+		if (!file_exists(IMAGES_DIR)) 
+			{
 			$oldumask = umask(0);
 			$ok = mkdir(IMAGES_DIR, 0777 );
 			umask($oldumask);
-		}
+			}
 		
 		// Changed this to only display Image files. This function
 		// is used in comment.php if the blog owner has img tag
 		// enabled for comments. (09/08/05 - alex)
-		$dir = IMAGES_DIR;
-		$contents = sb_folder_listing( $dir, array( '.jpg', '.jpeg', '.gif', '.png' ) );
+		$dir = BASEURL . "images/";
+		$contents = sb_folder_listing( IMAGES_DIR, array( '.jpg', '.jpeg', '.gif', '.png' ) );
 		// $contents = sb_folder_listing( $dir, array() );
 		
 		$str = NULL;
-		if ($contents) {
-			for ( $i = 0; $i < count( $contents ); $i++ ) {
-				$str  .= '<a href='.$dir.$contents[$i].' target=_blank>'.$contents[$i].'</a><br />';
+		if ($contents)
+			{
+			for ( $i = 0; $i < count( $contents ); $i++ )
+				{
+				if ($contents[$i] != '.htaccess' && $contents[$i] != 'Thumbs.db')
+					{
+					$str  .= '<img src="thumb.php?file=' . $contents[$i] . '"><br />';
+					$str  .= '<a href="'.$dir.$contents[$i].'" target=_blank>'.$contents[$i].'</a><br /><br />
+'; 
+					}
+				}
 			}
-		}
-		
 		return ( $str );
-	}
+		}
 ?>
