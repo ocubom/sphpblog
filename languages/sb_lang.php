6ad09126b08ef0bd97ba370e19ed1a70148122f7
nabber00 <nabber00@739c1e2e-722b-0410-a95f-adbfc7c39bab>
2012-03-30 17:20:27 +0000

    fix for processing multiple line msgids
    git-svn-id: https://svn.code.sf.net/p/sphpblog/svn/trunk@214 739c1e2e-722b-0410-a95f-adbfc7c39bab

--- languages/sb_lang.php
+++ languages/sb_lang.php
@@ -10,17 +10,26 @@ function sb_import_lang($name, $page="") {
 
   $handle = fopen(ROOT_DIR . "/languages/$name/strings.po", "r");
   $lastid = "";
+  $idmode = TRUE;
+  $strmode = FALSE;
   if ($handle) {
     while (($buffer = fgets($handle, 4096)) !== false) {
         //echo $buffer;
       if (preg_match("/msgid \"(.*)\"/", $buffer, $matches)) {
         $lastid = $matches[1];
+        $strmode = FALSE;
+        $idmode = TRUE;
       }
       if (preg_match("/msgstr \"(.*)\"/", $buffer, $matches)) {
         $lang_string[$lastid] = $matches[1];
+        $strmode = TRUE;
+        $idmode = FALSE;
       }
-      if (preg_match("/^\"(.*)\"/", $buffer, $matches)) {
+      if ($strmode AND preg_match("/^\"(.*)\"/", $buffer, $matches)) {
         $lang_string[$lastid] .= $matches[1];
+      }
+      if ($idmode AND preg_match("/^\"(.*)\"/", $buffer, $matches)) {
+        $lastid .= $matches[1];
       }
     }
     fclose($handle);
