e914384c1328079626fcb67508852ade06bb7697
nabber00 <nabber00@739c1e2e-722b-0410-a95f-adbfc7c39bab>
2012-05-24 20:05:22 +0000

    fix
    git-svn-id: https://svn.code.sf.net/p/sphpblog/svn/trunk@233 739c1e2e-722b-0410-a95f-adbfc7c39bab

--- parser.php
+++ parser.php
@@ -5,7 +5,11 @@
         // ---------------
         require_once('scripts/sb_functions.php');
         global $logged_in;
-        $logged_in = logged_in( true, true );
+        $logged_in = logged_in( false, false );
+
+	if (!$logged_in) {
+            exit;
+        }
 
         require_once('scripts/sb_header.php');
 
