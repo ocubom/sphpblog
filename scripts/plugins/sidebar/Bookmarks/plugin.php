3a44bccd34683b1168ef39db3590bbdcb7fdf42e
nabber00 <nabber00@739c1e2e-722b-0410-a95f-adbfc7c39bab>
2014-02-02 15:35:55 +0000

    fix for when using https
    git-svn-id: https://svn.code.sf.net/p/sphpblog/svn/trunk@261 739c1e2e-722b-0410-a95f-adbfc7c39bab

--- scripts/plugins/sidebar/Bookmarks/plugin.php
+++ scripts/plugins/sidebar/Bookmarks/plugin.php
@@ -66,7 +66,7 @@
 		
 		function getContent () {
 			$addthis = '<!-- AddThis Button BEGIN -->
-<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4c697ff844cb6c4f"><img src="http://s7.addthis.com/static/btn/sm-share-en.gif" width="83" height="16" alt="Bookmark and Share" style="border:0"/></a><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4c697ff844cb6c4f"></script>
+<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4c697ff844cb6c4f"><img src="//s7.addthis.com/static/btn/sm-share-en.gif" width="83" height="16" alt="Bookmark and Share" style="border:0"/></a><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4c697ff844cb6c4f"></script>
 <!-- AddThis Button END -->
 ';
 
