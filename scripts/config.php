2e58b25c8d3228d26eacf189eab0e06f3c5452a7
nabber00 <nabber00@739c1e2e-722b-0410-a95f-adbfc7c39bab>
2014-02-22 21:32:26 +0000

    version bump info
    git-svn-id: https://svn.code.sf.net/p/sphpblog/svn/trunk@272 739c1e2e-722b-0410-a95f-adbfc7c39bab

--- scripts/config.php
+++ scripts/config.php
@@ -65,8 +65,8 @@ print "<br>";
         // Last version and update information.
         //
         global $sb_info;
-        $sb_info[ 'version' ] = "0.8.1";
-        $sb_info[ 'last_update' ] = '07/23/12';
+        $sb_info[ 'version' ] = "0.8.3";
+        $sb_info[ 'last_update' ] = '02/22/14';
 
         //Remove timeout limit
         //@set_time_limit( 0 );
